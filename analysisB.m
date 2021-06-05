030e2539f48e17bb5fb97ea93baf5068eb2d1123
CPlant1 <n10465022@qut.edu.au>
2021-06-06 01:08:53 +1000

    Final updates part 1

--- analysisB.m
+++ analysisB.m
@@ -35,11 +35,16 @@ f13 = flight13.data;
 f17 = flight17.data;
 f25 = flight25.data;
 
-Nflights = 7;   %Manually set number of flights
-S = referenceSphere('earth');
-l_rw = zeros(1,Nflights);
+Nflights = 7;   %Hardcode number of flights
+S = referenceSphere('earth'); % Define sphere for mapping toolbox functions
+
+%PREALLOCATION of values that need to be extracted from the loop
+l_rw = zeros(1,Nflights);  
 heading_rw = zeros(1,Nflights);
 tf = zeros(1,Nflights);
+v_av = zeros(1,Nflights);
+pitch_rms = zeros(1,Nflights);
+
 % For loop to process data for every flight
 for i = 1:Nflights
     
@@ -95,15 +100,11 @@ for i = 1:Nflights
     caxis([0,max(round(gndSpeed))])
     cbar.Label.String = 'Ground Speed (m/s)';
 
-
- %cbarprop = get(cbar,'Title');
-
     subplot(3,2,3)
-    %plot(t, Alt, 'm--', 'LineWidth', 1.5);
     scatter(t, Alt, 10, SpeedColours, 'filled');
     grid on
     box on
-    title('Flight Side-view');
+    title('Flight Altitude');
     ylabel('Altitude [m]');
     xlabel('t [seconds]');
     colormap(cmap);
@@ -111,10 +112,7 @@ for i = 1:Nflights
     cbar.Ticks = linspace( 0, max(round(gndSpeed)), 5 );
     caxis([0,max(round(gndSpeed))])
     cbar.Label.String = 'Ground Speed (m/s)';
-    %set(gca,'FontSize',20);
-    
 
-    
     %Get the velocity components in each direction
     Vdown = zeros(1,Nsamps);
     Xeast = zeros(1,Nsamps);
@@ -126,30 +124,22 @@ for i = 1:Nflights
        %Down velocity = change in altitude / change in time
        Vdown(ii-1) = -((Alt(ii)- Alt(ii-1))/Tsamp);
        
-       %Convert degrees of latitude and longitude to N and E distances
-       %[dist(ii),angle(ii)] = distance(Lat(ii-1),Lon(ii-1),Lat(ii),Lon(ii),S);
+       %Use heading to find the east and north velocity components
        Veast(ii-1) = sin(deg2rad(heading(ii-1)))*gndSpeed(ii-1);
        Vnorth(ii-1) = cos(deg2rad(heading(ii-1)))*gndSpeed(ii-1);
-       
-       
-       
-%        xLon = distance(Lat(ii),Lon(ii-1),Lat(ii),Lon(ii),S);
-%        if (Lon(ii-1)> Lon(ii))
-%            xLon = -xLon;
-%        end
-%        
-%        Veast(ii-1) = (Xeast(ii)- Xeast(ii-1))/Tsamp;
-%        Vnorth(ii-1) = (Xnorth(ii)- Xnorth(ii-1))/Tsamp;
-       %az(ii-1) = azimuth(Lat(ii-1)-90,Lon(ii-1),Lat(ii)-90,Lon(ii));
+
     end
     
+    %Find moving average of the data
     Mdown = movmean(Vdown,10);
     Meast = movmean(Veast,10);
     Mnorth = movmean(Vnorth,10);
     
+    %Plot the three sets of velocity data on different axes
     subplot(3,2,2)
     %plot(t, Alt, 'm--', 'LineWidth', 1.5);   
     hold on, plot(t, Veast), plot(t, Meast)
+    legend("Raw Data","Moving Average")
     title('East Velocity');
     ylabel('Velocity [m/s]');
     xlabel('t [seconds]');
@@ -157,6 +147,7 @@ for i = 1:Nflights
     box on
     subplot(3,2,4)
     hold on,plot(t, Vnorth),plot(t, Mnorth)
+    legend("Raw Data","Moving Average")
     title('North Velocity');
     ylabel('Velocity [m/s]');
     xlabel('t [seconds]');
@@ -164,6 +155,7 @@ for i = 1:Nflights
     box on
     subplot(3,2,6)
     hold on,plot(t, Vdown),plot(t, Mdown)
+    legend("Raw Data","Moving Average")
     title('Down Velocity');
     ylabel('Velocity [m/s]');
     xlabel('t [seconds]');
@@ -171,19 +163,19 @@ for i = 1:Nflights
     box on
 
    %Find runway length
-   threshold = 1;
-   indexes_gnd = find(Alt-Alt(1) < threshold );
-   [~,index_takeoff] = max(diff(indexes_gnd));
-   index_land = index_takeoff + 1;
-   i_rw = indexes_gnd([index_takeoff,index_land]);
-   l_rw(i) =  distance(Lat(i_rw(1)),Lon(i_rw(1)),Lat(i_rw(2)),Lon(i_rw(2)),S);
-   Lat_rw = Lat(i_rw);
+   threshold = 1; %Below 1m altitude, the plane is considered grounded.
+   indexes_gnd = find(Alt-Alt(1) < threshold ); % find indexes where plane is grounded
+   [~,index_takeoff] = max(diff(indexes_gnd)); %find index of takeoff
+   index_land = index_takeoff + 1;  %index of landing must come straight after takeoff
+   i_rw = indexes_gnd([index_takeoff,index_land]);  %find indexes of the two runway extrema
+   l_rw(i) = distance(Lat(i_rw(1)),Lon(i_rw(1)),Lat(i_rw(2)),Lon(i_rw(2)),S); %use distance to calculate runway length
+   Lat_rw = Lat(i_rw); %Assign latitude and longitude values of runway extrema to a variable.
    Lon_rw = Lon(i_rw);
-   %set(gca,'FontSize',20);
+ 
    
    %find runway heading
    angle_rw = heading(1:i_rw(1));
-   angle_rw(gndSpeed(1:i_rw(1)) < threshold) = [];
+   %angle_rw(gndSpeed(1:i_rw(1)) < threshold) = [];
    heading_rw(i) = mean(angle_rw);
    
    
@@ -191,7 +183,9 @@ for i = 1:Nflights
    subplot(3,2,5)
    plot3(Lat, Lon, Alt)
    hold on
+   %set width of the runway (arbitrary)
    W_rw = 0.001;
+   %create runway rectangular plane for plotting
    runway_lat = [Lat_rw(1)-sin(deg2rad(heading_rw(i)))*W_rw, Lat_rw(1)+sin(deg2rad(heading_rw(i)))*W_rw, Lat_rw(2)+sin(deg2rad(heading_rw(i)))*W_rw, Lat_rw(2)-sin(deg2rad(heading_rw(i)))*W_rw, Lat_rw(1)-sin(deg2rad(heading_rw(i)))*W_rw];
    runway_lon = [Lon_rw(1)+cos(deg2rad(heading_rw(i)))*W_rw, Lon_rw(1)-cos(deg2rad(heading_rw(i)))*W_rw, Lon_rw(2)-cos(deg2rad(heading_rw(i)))*W_rw, Lon_rw(2)+cos(deg2rad(heading_rw(i)))*W_rw, Lon_rw(1)+cos(deg2rad(heading_rw(i)))*W_rw];
    plot3( runway_lat,runway_lon , [0,0,0,0,0],'k-' )
@@ -200,13 +194,10 @@ for i = 1:Nflights
     ylabel('y (m)');
     zlabel('z (m)');
    
-   
-   %sin(deg2rad(heading_rw(i)))*
-   
    %create annotations
    if i == 4
-    dim = [.15 .05 .3 .3];
-    str = 'Best Circuit Flown';
+    dim = [.15 .05 .3 .3];          
+    str = 'Best Circuit Flown';     
     annotation('textbox',dim,'String',str,'FitBoxToText','on');
    end
    
@@ -221,14 +212,15 @@ for i = 1:Nflights
     %Find down acceleration to find lift 
     a_lift(ii-1) = -(Vdown(ii)- Vdown(ii-1))/Tsamp; 
    end
-
+    
+   %save the final time of each simulation
    tf(i) = t(end);
-%     figure
-%     hold on
-%     plot(t,az) 
-%     plot(t,a_lift)
-%     plot(t,gndSpeed)
-%     plot(t,Alt)
+    
+    %find the pitch angle from the down velocity component of the velocity
+    pitch = atan((-Vdown)./(sqrt(Vnorth.^2 + Veast.^2)));
+    pitch(isnan(pitch)) = 0;
+    %Get RMS of pitch to account for negative values
+    pitch_rms(i) = rms(pitch);
 
 %Save values from 6th and 7th flights to estimate wind
     if i == 6
@@ -246,16 +238,16 @@ for i = 1:Nflights
         t_4 = t;
         v_4 = sqrt(Vnorth.^2 + Veast.^2 + Vdown.^2);
         yaw_4 = deg2rad(heading);
-        pitch_4 = atan((-Vdown)./(sqrt(Vnorth.^2 + Veast.^2)));
-        pitch_4(isnan(pitch_4)) = 0;
+        pitch_4 = pitch;
         
     end
-
+    %save the average velocity of each flight
+    v_av(i) = mean(gndSpeed);
 end
 
-l_rw_max  = max(l_rw); 
-h_rw = mean(heading_rw);
-tf_av = mean (tf);
+l_rw_max  = max(l_rw); % max length between take off and touch down (rw length)
+h_rw = mean(heading_rw); %find average runway heading
+tf_av = mean (tf);  %find average duration of flight
 
 %Estimate wind direction and magnitude 
 %Graphically find 2 indices of wind [tailwind, headwind]
@@ -293,9 +285,9 @@ Nsamps_p = length(t_p);
 %velocity vector
 %perfect top speed is 150kts = 77m/s
 %accelerate to top spee quickly and then decelerate quickly
-v_p = [linspace(0,47.7,round(Nsamps_p/10)) ...
-     47.7*ones(1,round(4*Nsamps_p/5))...
-     linspace(47.7,0,round(Nsamps_p/10))];
+v_p = [linspace(0,48.5,round(Nsamps_p/10)) ...
+     48.5*ones(1,round(4*Nsamps_p/5))...
+     linspace(48.5,0,round(Nsamps_p/10))];
 
 %pitch vector 
 %critical airfoil angle is 15 for many aircraft
--- simple3dof.m
+++ simple3dof.m
@@ -1,31 +1,23 @@
 function [f1,f2] = simple3dof(t,v,yaw,pitch)
-%circuit sim creates a basic 3DOF simulation of an aircraft
-%   Detailed explanation goes here
-%t: time vector
-%v: velocity vector
-%yaw: yaw vector
-%pitch: pitch vector
-%pos: starting position (Lat, Lon, Alt)
 
-dt = t(2)-t(1); % Time step (sampling time)
-Nsamps = length(t);
-tf = t(end);
 
-%TODO: initialise the state vectors (x,y and z) with the airplane position
+dt = t(2)-t(1); %sampling time
+Nsamps = length(t); %number of samples
+tf = t(end); %find final simm time
+
+%preallocate the output vectors
 x = zeros(1,length(t));
 y = zeros(1,length(t));
 z = zeros(1,length(t));
-%% (Task 3) Simulation 
-%TODO: Simulate the 2D trajectory based on the speed and yaw vectors
-% (hint: use a for-loop)
+
+%This for loop processes the 3DOF dynamics of the aircraft
 for i = 2:Nsamps
-    %TODO: Implement the 2D kinematic equations of motion for every sample
     x(i) = x(i-1) + dt*v(i-1)*cos(pitch(i-1))*cos(yaw(i-1));
     y(i) = y(i-1) + dt*v(i-1)*cos(pitch(i-1))*sin(yaw(i-1));
     z(i) = z(i-1) + dt*v(i-1)*sin(pitch(i-1));
 end
-%% (Task 4) Plotting 
-%TODO: Plot the 2D (x,y) trajectory
+
+%plot 3D trajectory
 f1 = figure; hold off
     plot3(x,y,z,'LineWidth',1); grid on
     title('3D trajectory');
@@ -34,24 +26,6 @@ f1 = figure; hold off
     zlabel('z (m)');
     axis equal
 
-%TODO: Create a two-axis plot showing the airplane's yaw (left) and 
-% x position (left) versus time.
-% f2 = figure; hold off
-%     clf; hold on;
-%     yyaxis left
-%     plot(t,yaw);
-%     ylabel('Yaw (rad)');
-% %     ylim([-200,800]);
-%     yyaxis right
-%     plot(t,x, '--');
-%     ylabel('x (m)');
-% %     ylim([-200,800]);
-%     hold off;
-%     grid on;
-%     
-%     title('Flight Yaw and Position in x');
-%     xlabel('Time (s)');
-%     xlim([0,tf]);
 
 end
 
